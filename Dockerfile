8110efaf6dd035798264cb710b90bfa0d898d657
Andre Bueno <andre.bueno@accenture.com>
2022-11-16 19:35:20 -0300

    add teste configuration

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /home
 COPY main.py .
 
 RUN pip install fastapi \
-   && pip install "uvicorn[standard]"
+   && pip install "uvicorn[standard]" \
+   && pip install kafka-python
 
 ENTRYPOINT [ "uvicorn","main:app","--reload", "--port", "8080", "--host", "0.0.0.0"]
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from kafka import KafkaProducer
+import os, random
+
 app = FastAPI()
+kafka_server = os.getenv('KAFKA_SERVER')
+kafka_port = os.getenv('KAFKA_PORT')
+kafka_url = f'{kafka_server}:{kafka_port}'
+
+producer = KafkaProducer(bootstrap_servers=[kafka_url])
 
 @app.get("/")
 async def root():
-    return {"hello message": "Hello World"}
+    number = random.randint(0,9999999)
+    producer.send('Api', number)
+    return {"number": number}
