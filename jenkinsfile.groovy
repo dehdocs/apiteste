120df8443ec7c5fd6dce7b3ebff770cd3eb296a6
Andre Bueno <andre.bueno@accenture.com>
2022-11-16 16:36:27 -0300

    add teste configuration

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,16 +1,18 @@
 node('docker-node') {
-  def name_img = "${env.JOB_NAME}".split('/').last().toLowerCase()
+  def name_img = "msteste:1.0.1"
   stage('Prepare') {
     checkout scm
   }
   stage('build'){
-    docker.withRegistry('http://20.231.125.187:8182', 'nexus') {
+    docker.withRegistry('https://registry.devops.7f8254f4188647b4be19.eastus.aksapp.io', 'nexus') {
       def dockerImage = docker.build("$name_img")
       dockerImage.push()
     }
   }
   stage('deploy'){
-    
+    withCredentials([file(credentialsId: '	kubeconfig', variable: 'SECRET_FILE')]) {
+      sh 'KUBECONFIG=$SECRET_FILE kubectl apply -f deployment.yaml'
+    }
   }
 
 }
